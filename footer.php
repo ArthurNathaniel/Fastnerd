977ddfe3c8db1e3b812c2bef94a36e4801e5253e
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-07-13 12:06:35 -0500

    chhh

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <footer>
    <div class="footer-grid">
       <div class="foot-abt">
-         <h2>About Us</h2>
+         <h2 style="color: #fff;">About Us</h2>
          <p>
             We believe that all those who come to us must be served with
             integrity. As such, our clients are assured of reliable services.
          </p>
       </div>
       <div class="foot-services">
-         <h2>Services</h2>
+         <h2 style="color: #fff;">Services</h2>
          <a href="index.php">Digital & Outdoor</a>
 
          <a href="./web/index.html">Web & Systems</a>
